2d2d3e53f7d661161034ac002a15803195528132
HarrisMusungu <hmusungu@ashland.edu>
2024-05-27 23:11:36 -0400

    New look Update

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-    new fullpage('#fullpage', {
-        autoScrolling: true,
-        navigation: true,
-        navigationPosition: 'right',
-        navigationTooltips: ['Intro', 'About Me', 'Resume', 'Projects', 'Fun Facts', 'Contact'],
-        showActiveTooltip: true,
-        sectionsColor: ['#333', '#444', '#555', '#666', '#777'], // Different shades of dark for each section
-        scrollHorizontally: true,
-        css3: true,
-        easingcss3: 'ease-in-out'
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        },
+        loop: true
     });
 });
 
